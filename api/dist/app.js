82d86e9c0f650cc134cc70fc537c9ef05bc22627
Agustin Puggioni <agusinpuggioni98@gmail.com>
2022-11-10 10:26:20 -0300

    cors

--- api/dist/app.js
+++ api/dist/app.js
@@ -27,7 +27,9 @@ app.use((_req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE,PATCH");
+    res.header("Access-Control-Allow-Methods", "*"
+    //"GET, POST, OPTIONS, PUT, DELETE,PATCH"
+    );
     res.header("Access-Control-Allow-Information", "*");
     next();
 });
--- api/src/app.ts
+++ api/src/app.ts
@@ -29,7 +29,8 @@ app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Headers", "*");
   res.header(
     "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, DELETE,PATCH"
+    "*"
+    //"GET, POST, OPTIONS, PUT, DELETE,PATCH"
   );
   res.header("Access-Control-Allow-Information", "*");
   next();
